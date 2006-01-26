b479c5cfc89f0fddbe09d9a418af3490390c9b91
silviamartina <silviamartina>
2006-01-26 16:39:38 +0000

    javadoc prueba

--- plg/procesador/Atributos.java
+++ plg/procesador/Atributos.java
@@ -1,5 +1,13 @@
 package procesador;
-
+/**
+ * La clase <B>Atributos</B> almacena los atributos "err" y "tipo" de las gramáticas
+ * <P>Los atributos que tiene esta clase son los siguientes:
+ * <UL><LI><CODE>err:</CODE> boleano que nos indica si hubo un error o no.</LI>
+ * <LI><CODE>tipo:</CODE> string donde se almacena el tipo: int o bool.</LI></UL></P>
+ * 
+ * @author Paloma de la Fuente, Jonás Andradas, Leticia García y Silvia Martín
+ *
+ */
 public class Atributos {
 
 	/*
@@ -11,23 +19,21 @@ public class Atributos {
 	boolean err;
 	String tipo;
 	
-	/*
-	 * Parametros de entrada: 
-	 * Parametros de salida:
-	 * 
+	/**
 	 * Constructor sin parametros.
 	 */
 	public Atributos() {
 		super();
 	}
 	
-	/*
-	 * Parametros de entrada: Boleano que nos indica si ha sucedido un error y,
-	 * 						String con el tipo del identificador, expresion o valor.
-	 * Parametros de salida:
-	 * 
+	/**
 	 * Constructor que inicializa los atributos de Atributos con los valores que recibe
 	 * por parametro.
+	 * 
+	 * @param err Booleano que nos indica si ha sucedido un error y,
+	 * @param tipo String con el tipo del identificador, expresion o valor.
+	 * Parametros de salida:
+	 * 
 	 */
 	public Atributos(boolean err, String tipo) {
 		super();
@@ -35,8 +41,9 @@ public class Atributos {
 		this.tipo = tipo;
 	}
 	
-	/*
+	/**
 	 * Accesores y mutadores de los atributos de la clase.
+	 * @return
 	 */
 	public boolean getErr() {
 		return err;
