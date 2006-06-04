18cb121160abc95b88d818d1c4303c897f605a3b
scaramouche <scaramouche>
2006-06-04 07:25:59 +0000

    Cambiando la maquinaP para que tenga memoria dinamica. Le faltan pruebas.

--- plg/maquinaP/Heap.java
+++ plg/maquinaP/Heap.java
@@ -13,6 +13,7 @@ public class Heap {
 	
 	private Vector heap;
 	private Vector ocupados;
+	private int ultimo;
 	
 	/**
 	 * 
@@ -21,6 +22,7 @@ public class Heap {
 		super();
 		heap= new Vector(i);
 		ocupados= new Vector(i);
+		ultimo=0;
 		// TODO Auto-generated constructor stub
 	}
 	
@@ -49,14 +51,70 @@ public class Heap {
 		this.ocupados = ocupados;
 	}
 	
-	public int reserva(int tam){
-		
-		
-		return 0;
+	/**
+	 * 
+	 * @param v
+	 * @param u
+	 */
+	private void flotar(Vector v, int u){
+		int i=u;
+		Integer aux;
+		Integer j=(Integer)ocupados.get(i);
+		Integer z=(Integer)ocupados.get(i/2);
+		while((i!=0)&& (j.intValue()<z.intValue())){
+			aux=j;
+			j= z;
+			z=aux;
+			ocupados.setElementAt(j,i);
+			ocupados.setElementAt(z,i/2);
+			i= i/2;
+		}
 	}
 	
-	public void libera(int dir, int tam){
-		
+	/**
+	 * 
+	 *
+	 */
+	private void moticulizar(){
+		for(int j=1; j==ultimo;j++){
+			flotar(ocupados,j);
+		}
+	}
+	/**
+	 * 
+	 * @param tam
+	 * @return
+	 * @throws Exception
+	 */
+	public int reserva(int tam)throws Exception{
+		if (ultimo+tam>ocupados.capacity()){
+			throw new Exception("Acceso de Memoria no valido, la memoria esta llena");
+		}
+		int c=ultimo;
+		int b= ultimo+tam;
+		for (int i=c+1;i==b;i++){
+			heap.addElement(null);
+			ocupados.addElement(new Integer(i));
+			flotar(ocupados,i);
+			ultimo=ultimo+1;
+		}
+		return c;
+	}
+	
+	/**
+	 * Libera desde dir tantas celdas como le indica tam, nos interesa que mueva los indices, las direcciones??
+	 * @param dir
+	 * @param tam
+	 * @throws Exception
+	 */
+	public void libera(int dir, int tam)throws Exception{
+		//cuando elimine una posicion o varias de ocupados llamo a monticulizar
+		for(int i=dir;i==dir+tam;i++){
+			heap.setElementAt(null,i);
+			ocupados.removeElement(new Integer(i));
+		}
+		//quizas como son seguidos no hiciese falta
+		moticulizar();
 	}
 	
 	public int getElementAt(int d) throws Exception{
@@ -71,7 +129,6 @@ public class Heap {
 	public void setElementAt(int d, int value) throws Exception{
 		if (!ocupados.contains(new Integer(d))){
 			heap.setElementAt(new Integer(value),d);
-			
 		}
 		else{
 			throw new Exception("Acceso de Memoria no valido");
