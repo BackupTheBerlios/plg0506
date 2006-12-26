e62965502d03532dfda8b99084aa8558ca2607b7
leticia <leticia>
2006-12-26 21:00:07 +0000

    Initial revision

---
+++ cvs/plg/plgLP/maquinaP/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Paquete <B>MaquinaP</B> donde esta contenido de la maquina virtual creada para el leguaje compilado por nuestra práctica. Ademas también contiene
+ * la clase Codigo la cual genera un fichero .obj con el codigo ejecutable por la clase MaquinaP. La clase Codigo genera un String que se muestra por 
+ * pantalla el resultado de ejecutar el ese codigo. Durante el analisis almacenamos el el fichero .obj error y el tipo en un elemento llamado 
+ * Atributos y que también podemos obtener mas informacion en la propia clase.
+ * 
+ * @author Jonás Andradas, Paloma de la Fuente, Leticia García y Silvia Martín
+ */
+package maquinaP;
---
+++ cvs/plg/plgLP/procesador/Procesador.java
@@ -0,0 +1,45 @@
+package procesador;
+import java.io.RandomAccessFile;
+
+import javax.swing.JOptionPane;
+
+import tablaSimbolos.TablaSimbolos;
+
+/**
+ * La clase <B>Procesador</B> es el procesador del compilador del lenguaje. Se encarga de crear la Tabla de Simbolos,
+ * de llamar a Sintactico, para empezar el analisis.
+ * 
+ * <P>No tiene atributos</P>
+ * 
+ * @author Jonás Andradas, Paloma de la Fuente, Leticia García y Silvia Martín
+ *
+ */
+public class Procesador {
+
+	/**
+	 * El método procesa de esta clase implementa del procesador del lenguaje y captura una excepción
+	 * IOException procedente de la función de RandomAccessFile de JAVA si no se encuentra el fichero de entrada.
+	 * 
+	 * @param fuente Que contendrá la ruta del fichero a procesar.
+	 * @param f String con la ruta del fichero.
+	 * 
+	 */
+	public void procesa(RandomAccessFile fuente, String f){
+		
+			try {
+				/*
+				 * Tratamos de realizar todas las operaciones, si alguna falla y genera excepcion
+				 * se recoge mas abajo.
+				 */
+				TablaSimbolos TS = new TablaSimbolos();	
+				Sintactico sintactico;		
+				sintactico = new Sintactico(fuente, TS, f);
+				System.out.println("Inicio del analisis\n");
+				sintactico.startParsing();
+				System.out.println("Fin del analisis");
+			} 
+			catch (Exception e) {
+				JOptionPane.showMessageDialog(null,e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+			}
+	}
+}
