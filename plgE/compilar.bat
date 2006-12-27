357e2bc41206e911fa952336e1125d89028d33ad
velazquez <velazquez>
2006-12-27 16:49:24 +0000

    Practica de Emi.

---
+++ plgE/compilar.bat
@@ -0,0 +1,8 @@
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\Constantes.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\tablaSimbolos.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\AnalizadorLexico.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\analizadorSin.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\MaquinaP.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\resultadoAnalisis.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\CompiladorGUIImp.java
+javac -classpath .\bin -sourcepath .\src -d .\bin .\src\Main.java
