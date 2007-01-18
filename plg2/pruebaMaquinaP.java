462062d7f6e69538fabbb39721abcd7fd0700848
scaramouche <scaramouche>
2007-01-18 21:58:12 +0000

    mmm

--- plg2/pruebaMaquinaP.java
+++ plg2/pruebaMaquinaP.java
@@ -8,7 +8,7 @@ public class pruebaMaquinaP {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-			String f = new String ("Ejemplo3.txt");
+			String f = new String ("Ejemplo.txt");
 			File file = new File(f);
 			Procesador p = new Procesador();
 			p.procesa(file);
