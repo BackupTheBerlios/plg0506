23bb2560ea52c4b332d6118ba1680971853deeb1
velazquez <velazquez>
2008-01-22 09:31:57 +0000

    : 	plg08/plg08/.settings/org.eclipse.core.resources.prefs

--- plg08/plg08/maquinaP/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Paquete <B>MaquinaP</B> donde esta contenido de la maquina virtual creada para el leguaje compilado por nuestra 
- * practica. 
- * 
- * @author Paloma de la Fuente, Leticia Garcia, Ines Gonzalez, Emilia Rodriguez y Alberto Velazquez
- */
-package maquinaP;
