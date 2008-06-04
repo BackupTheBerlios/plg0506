b96bdb7065e0b798bb6c67b22c3445c2e5140f96
fede <fede>
2008-06-04 16:29:28 +0000

    tipos tiene ahora un toString de puta madre

--- plg3/tablaSimbolos/Tipo.java
+++ plg3/tablaSimbolos/Tipo.java
@@ -1,5 +1,7 @@
 package tablaSimbolos;
 
+import java.util.Hashtable;
+
 /**
  * 
  * @author Paloma de la Fuente, Ines Gonzalez, Federico G. Mon Trotti, Nicolas Mon Trotti y Alberto Velazquez
@@ -7,12 +9,64 @@ package tablaSimbolos;
  */
 public class Tipo {
 
-	String t; //BOOLEAN, INTEGER, ref, rec, array, 
+	public enum tipo {BOOLEAN, INTEGER, ref, rec, array}
+	tipo t; //BOOLEAN, INTEGER, ref, rec, array, 
 	String id;
 	int tam;
-	Tipo tbase;
-	int campos;
+	int nElems;
+	Tipo tBase;
+	Hashtable<Object,Object> campos;
 	int desplazamiento;
 	
 	
+	/**
+	 * Metodo para comprobar si existe ya un identificador declarado en los campos del record. Si es asi
+	 * devolvera verdadero para que se genere un error.
+	 * 
+	 * @param id Identificador de variable que se va a comprobar si exite o no en el record.
+	 * @return boolean Sera verdadero si el identificador si existe y falso en otro caso.
+	 */
+	public boolean existeCampo(String id)throws Exception{
+		return this.campos.containsKey(id);
+	}
+	
+	/**
+	 * Metodo para añadir un nuevo identificador a el record. 
+	 * 
+	 * @param id Nombre del identificador que se va añadir.
+	 * @param tipo Tipo del nuevo identificador a añadir.
+	 * @return boolean Se devuelve verdadero si todo ha sido correcto. Falso en caso contrario.
+	 * @throws Exception Excepcion generada si el identificador ya existe, se capturara en otro lugar.
+	 */
+	public boolean addCampo(String id, Tipo tipo) throws Exception{
+		if (this.campos.containsKey(id)){
+			throw new Exception ("No se puede duplicar el identificador");
+		}
+		else{
+			this.campos.put(id,tipo);
+			return true;	
+		}	
+	}
+	
+	
+	public String toString(){
+		String toString = t.toString();
+		toString += " "+id;
+		toString += " "+Integer.toString(tam).toString();		
+		switch (t){ 
+			case ref:
+				toString += tBase.toString();
+				break;
+			case array:
+				toString += Integer.toString(nElems);
+				toString += tBase.toString();
+				break;
+			case rec:
+				toString += Integer.toString(nElems);
+				toString += campos.toString();
+				toString += Integer.toString(desplazamiento);
+				break;
+		}
+		return toString;
+	}
 }
