33551c99652dce1b9e4b34a5ff02869de08fc26e
fede <fede>
2008-06-04 15:15:31 +0000

    Creado Tipo con lo básico y mal

---
+++ plg3/tablaSimbolos/Tipo.java
@@ -0,0 +1,18 @@
+package tablaSimbolos;
+
+/**
+ * 
+ * @author Paloma de la Fuente, Ines Gonzalez, Federico G. Mon Trotti, Nicolas Mon Trotti y Alberto Velazquez
+ * 
+ */
+public class Tipo {
+
+	String t; //BOOLEAN, INTEGER, ref, rec, array, 
+	String id;
+	int tam;
+	int tbase;
+	int campos;
+	int desplazamiento;
+	
+	
+}
